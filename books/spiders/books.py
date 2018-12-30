0361140d9acdf77e95a731e520b08ba1c59c7f11
Butch Ewing <butch@butchewing.com>
2018-12-29 22:54:39 -0600

    fixing keys

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -26,28 +26,28 @@ class BooksSpider(scrapy.Spider):
         item['url'] = response.url
         item['slug'] = urlsplit(response.url).path
         item['directories'] = ', '.join([x for x in urlsplit(response.url).path.split('/')[1:]]),
-        item['title'] = response.xpath('//title/text()').extract()
+        item['meta_title'] = response.xpath('//title/text()').extract()
+        item['meta_description'] = response.xpath("//meta[@name='description']/@content")[0].extract()
+        item['load_time'] = response.meta['download_latency']
+        item['status_code'] = response.status
         item['h1'] = response.xpath('//h1//text()').extract()
         item['h2'] = response.xpath('//h2//text()').extract()
         item['h3'] = response.xpath('//h3//text()').extract()
         item['h4'] = response.xpath('//h4//text()').extract()
-        item['description'] = response.xpath("//meta[@name='description']/@content")[0].extract()
         item['img_links'] = 0
         item['img_alt_tags'] = 0
         item['img_count'] = 0
 #        item['link_urls'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "btTextLeft", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "bt_bb_wrapper", " " ))]//a/@href').extract() 
 #        item['link_text'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "btTextLeft", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "bt_bb_wrapper", " " ))]//a/text()').extract()
-#        item['link_count'] = len(item['link_urls'])        
-        item['load_time'] = response.meta['download_latency']
-        item['status_code'] = response.status 
+#        item['link_count'] = len(item['link_urls'])
         
         product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
+        item["product_title"] = product.css("h1 ::text").extract_first()
+        item['product_category'] = response.xpath(
             "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
         ).extract_first()
-        item['description'] = response.xpath(
+        item['product_description'] = response.xpath(
             "//div[@id='product_description']/following-sibling::p/text()"
         ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item['product_price'] = response.css('p.price_color ::text').extract_first()
         yield item
